67d3f44a6fc4346dbd13024bb5f48e5daea2fa8b
Natnael Wondwoesn <ninatina423@gmail.com>
2023-11-21 20:36:12 +0300

    2ND Commit

--- src/components/Jokes/Jokes.jsx
+++ src/components/Jokes/Jokes.jsx
@@ -12,9 +12,14 @@ function Jokes(){
         <div>
             <h1 className="font-black m-2 text-4xl">Wanna Hear A Corney Joke ?</h1>
             <div>
-                <button className="bg-black hover:bg-blue-400 text-white font-bold py-2 px-4 rounded m-3" onClick={getJoke}>Jokes </button>
-                <p className="p-2">{joke}</p>
+                <button className="flex justify-center items-center bg-black hover:bg-blue-400 text-white font-bold py-2 px-4 rounded m-3"
+                        onClick={getJoke}>
+                    Click Me !!! </button>
 
+
+            </div>
+            <div className="">
+                <p className="p-2">{joke}</p>
             </div>
     </div>
     )
